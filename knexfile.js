ad1108bf9b4be607affdc092f8d178f08f5d1e1d
Victoria Daug <victoria.daug@gmail.com>
2018-03-16 21:36:00 +0000

    changes in knexfile

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   test: {
     client: 'pg',
-    connection: process.env.TEST_DATABASE_URL || 'postgres://dev:dev@localhost/noteful-test',
+    connection: process.env.TEST_DATABASE_URL || 'postgres://localhost/noteful-test',
     pool: {min : 1 , max : 2}
   },
   production: {
